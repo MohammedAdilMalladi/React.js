a59161e317288d14114a36be5c92854872d05627
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-19 11:50:00 +0530

    implemented evaluated expression and made changes to main file

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,12 @@
 import Learn from "./learn";
 
+
 function App() {
+  const username = "react"
   return (
     <>
-    <h1>This h1 tag is from the app function</h1>
+    <h1>This h1 tag is from the app function {username}</h1> 
+    {/* {username} - here it is called as evaluated expression */}
     <Learn />
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Google</a>
 )
+
+const User = "| react"
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://youtube.com', target:'_blank' },
+  'click here to visit YouTube',
+      User
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
+  render(
+
+    <MyApp />
+    anotherElement
+    reactElement
+    <App />
+    
+  )
