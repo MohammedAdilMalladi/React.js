eea182464deaf2bba5cbac224ead217f8bded6e3
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-20 16:21:12 +0530

    Completed basic foundation of reactjs

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './components/Card.jsx'
+
+function App() {
+  
+  return (
+    <>
+    <h1 className="bg-yellow-400 text-2xl text-black p-5 rounded-4xl">TailwindCSS</h1>
+    <Card username="Get new images" btn="Click here to learn more" /> 
+    <Card username="Explore new images" />
+    </>
+  )
+}
+
+export default App
