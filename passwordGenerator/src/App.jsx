b5ec41e60a7ab14c685e6f933ed82019e53e9d71
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-23 15:00:00 +0530

    backdated commit

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -25,7 +25,20 @@ function App() {
 
   return (
     <>
-    <div className='w-full max-w-md text-center mx-auto shadow-md rounded-lg px-4  my-8 bg-gray-500 text-white-700'>Password Generator</div>
+    <div className='w-full max-w-md text-center mx-auto shadow-md rounded-lg p-5 my-8 bg-gray-500 text-white-700 text-white'>
+      <p className='text-2xl font-medium'>Password Generator</p>
+      <div className='flex-shadow flex justify-center rounded-lg overflow-hidden mb-4 p-5'>
+
+        <input 
+        type="text"
+        value={password}
+        className="outline-none w-full py-1 px-3 bg-white text-black rounded-l-md"
+        placeholder="Password"
+        readOnly
+         />
+        <button className='bg-blue-700 px-3 py-0.5 shrink-0 cursor-pointer rounded-r-md'>copy</button>        
+      </div>
+    </div>
     </>
   )
 }
