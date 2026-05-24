b32019c0319c3a0e850597c96b3bfdb2cb72c92d
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-24 14:50:40 +0530

    Built Password Generator

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,45 +1,101 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect,useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [length, lengthAllowed] = useState(8);
+  const [length, setLength] = useState(8);
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if(numAllowed) str += "0123456789";
-    if(charAllowed) str += "!@#$%^&*+=";
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*+=";
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random * str + 1);
-      pass = str.charAt(char);
-      
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+
     }
 
     setPassword(pass);
   }, [length, numAllowed, charAllowed, setPassword]);
 
-  return (
-    <>
-    <div className='w-full max-w-md text-center mx-auto shadow-md rounded-lg p-5 my-8 bg-gray-500 text-white-700 text-white'>
-      <p className='text-2xl font-medium'>Password Generator</p>
-      <div className='flex-shadow flex justify-center rounded-lg overflow-hidden mb-4 p-5'>
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 99);
+    window.navigator.clipboard.writeText(password);
+  }, [password])
 
-        <input 
-        type="text"
-        value={password}
-        className="outline-none w-full py-1 px-3 bg-white text-black rounded-l-md"
-        placeholder="Password"
-        readOnly
-         />
-        <button className='bg-blue-700 px-3 py-0.5 shrink-0 cursor-pointer rounded-r-md'>copy</button>        
-      </div>
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numAllowed, charAllowed, passwordGenerator]);
+
+  return (
+    <div className='flex items-center justify-center mt-48'>
+        <div className='w-full max-w-lg text-center shadow-md rounded-lg p-5 bg-gray-700 text-white'>
+
+          <p className='text-2xl font-medium'>Password Generator</p>
+
+          <div className='flex-shadow flex justify-center rounded-lg overflow-hidden mb-4 p-5'>
+            <input
+              type="text"
+              value={password}
+              className="outline-none w-full py-1 px-3 bg-white text-black rounded-l-lg"
+              placeholder="Enter Password"
+              readOnly
+              ref={passwordRef}
+            />
+            <button 
+            onClick={copyPasswordToClipboard}
+            className='bg-blue-700 px-3 py-0.5 shrink-0 cursor-pointer rounded-r-lg'>copy</button>
+          </div>
+
+          <div className='flex justify-center text-md gap-x-3'>
+
+            <div className='flex items-center gap-x-1'>
+              <input
+                type="range"
+                min={6}
+                max={100}
+                value={length}
+                className='cursor-pointer'
+                onChange={(e) => { setLength(e.target.value) }}
+              />
+              <label>Length: ({length})</label>
+            </div>
+
+            <div className='flex items-center gap-x-1'>
+              <input
+                type="checkbox"
+                defaultChecked={numAllowed}
+                className='cursor-pointer'
+                onChange={() => {
+                  setNumAllowed((prev) => !prev);
+                }}
+              />
+              Number
+            </div>
+
+            <div className='flex items-center gap-x-1'>
+              <input
+                type="checkbox"
+                defaultChecked={charAllowed}
+                className='cursor-pointer'
+                onChange={() => {
+                  setCharAllowed((prev) => !prev);
+                }}
+              />
+              Special Characters
+            </div>
+
+          </div>
+        </div>
     </div>
-    </>
   )
 }
 
