4166229daa99c99d0c732db479f7d30c85d5e3af
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-25 14:00:00 +0530

    worked on customHooks

---
+++ customHooks/src/hooks/useCounter.js
@@ -0,0 +1,13 @@
+// What is a Custom Hook?
+// A custom hook is a normal JavaScript function that uses React hooks (useState, useEffect, etc.) to reuse logic across multiple components.
+
+// Why Custom Hooks Are Used
+
+// Without custom hooks:
+// same logic gets repeated in many components
+
+// With custom hooks:
+// write logic once
+// reuse everywhere
+// cleaner code
+// easier maintenance
