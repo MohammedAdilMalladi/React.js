6e4141f4e47a37f5f8a3a43b2a6ecbe44775e159
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-05-29 09:00:00 +0530

    Created Router

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './components/Header/Header.jsx'
-import Home from './components/Home/Home.jsx'
-import Footer from './components/Footer/Footer.jsx'
 import { RouterProvider } from 'react-router-dom'
+import Route from './Route.jsx'
 
-
+const router = createBrowserRouter([
+  path: '/',
+  element: <Route/>,
+  children: [
+    {
+      path: "",
+      element: <Home />
+    },
+  ]
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
