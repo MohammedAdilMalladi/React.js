46d6380c3ddb9c12fa1b9dea59cff90507ff9df5
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-06-02 11:38:43 +0530

    Resolved Errors

--- 07reactRouter/src/components/About/About.jsx
+++ 07reactRouter/src/components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src="https://images.pexels.com/photos/8188062/pexels-photo-8188062.jpeg"
                             alt="image"
                         />
                     </div>
