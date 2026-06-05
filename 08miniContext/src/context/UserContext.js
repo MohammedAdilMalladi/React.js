58ec49adc2332f5affd4ae045b0f6fc2a6a33965
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-06-05 10:00:00 +0530

    Revised Context API and made some changes

--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -1,3 +1,5 @@
+// Define Context API - Context API is a React feature that allows data to be shared across multiple components without passing props manually through every level of the component tree.
+
 import React from 'react'
 
 const UserContext = React.createContext()
