b04c6134673674912435c7056a612cd8ddfbd843
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-06-09 14:00:00 +0530

    Context API with Local storage

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,6 +1,18 @@
 import {createContext, useContext} from 'react'
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Learn ReactJS",
+            completed: false
+        },
+    ],
+    addTodo: (id) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 export const useTodo = () => {
     return useContext(TodoContext)
