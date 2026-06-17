b83c0ef0beb6f4c62c37658e79945783ceae04c7
Mohammed Adil Malladi <adilmalladi6861@gmail.com>
2026-06-17 17:00:00 +0530

    added files

--- 11reduxToolkitTodo/src/main.jsx
+++ 11reduxToolkitTodo/src/main.jsx
@@ -1,8 +1,8 @@
-import { Provider } from 'react-redux'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { store } from './app/store'
-import App from './App.jsx'
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { store } from './app/store';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
